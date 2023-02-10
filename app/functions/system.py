b44c038a6e417da8f4a62100b3876a28dc7460ac
jhonjab19 <jhonjairob119g@gmail.com>
2023-02-10 14:48:41 -0500

    Correccion en funcion de obtener IP

--- app/functions/system.py
+++ app/functions/system.py
@@ -1,10 +1,10 @@
 
-def get_IP(request):
+def get_IP(request: dict):
    'Obtiene la dirección IP del usuario'
-   client_ip = request.META.get('HTTP_X_FORWARDED_FOR', None)
+   client_ip = request.get('HTTP_X_FORWARDED_FOR', None)
    if client_ip is None:
-      client_ip = request.META.get('HTTP_X_REAL_IP', None)
+      client_ip = request.get('HTTP_X_REAL_IP', None)
    if client_ip is None:
-      client_ip = request.META.get('REMOTE_ADDR', None)
+      client_ip = request.get('REMOTE_ADDR', None)
    return client_ip
 
