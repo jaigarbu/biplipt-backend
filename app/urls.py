c63a178370d9b1b2dd19c99f3cbd4c7ec93cf229
jhonjab19 <jhonjairob119g@gmail.com>
2023-03-02 16:49:11 -0500

    Add: soporte carga de archivos en GraphQL y uuid

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from graphene_file_upload.django import FileUploadGraphQLView
 
 from app.graphql.schema import Schema
 
 urlpatterns = [
-   path('data/api/', csrf_exempt(GraphQLView.as_view(graphiql=False, schema=Schema)))
+   path('data/api/', csrf_exempt(FileUploadGraphQLView.as_view(graphiql=False, schema=Schema)))
 ]
